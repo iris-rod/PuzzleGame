5d43087fe8abbad1c6a6f43b443bb6fbc9b53c4a
iris410 <igprodrigues125@gmail.com>
2021-01-03 18:56:08 +0000

    Add some more consts

--- PuzzleGame/PuzzleGame/Source/Consts.h
+++ PuzzleGame/PuzzleGame/Source/Consts.h
@@ -1,8 +1,12 @@
 #pragma once
 
-static const int MAP_SIZE_X = 10;
-static const int MAP_SIZE_Y = 7;
+static const int TOTAL_COLUMNS = 15; //10
+static const int TOTAL_ROWS = 7; //7
+static const int INITIAL_COLUMNS = 10;
 static const int PIECE_SIZE_X = 32;
 static const int PIECE_SIZE_Y = 32;
 
+static const int NEW_COLUMN_TIME = 6; //in seconds
 
+static const int START_X = 300;
+static const int START_Y = 50;
