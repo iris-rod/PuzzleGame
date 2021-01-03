2b408d21b417f352d63a8c5ad50cdf7ddfcb56ea
iris410 <igprodrigues125@gmail.com>
2021-01-03 18:57:07 +0000

    Allow creation of empty piece

--- PuzzleGame/PuzzleGame/Source/Ore.cpp
+++ PuzzleGame/PuzzleGame/Source/Ore.cpp
@@ -1,8 +1,11 @@
 #include "Ore.h"
 
-Ore::Ore(int Id, SDL_Rect* _src, SDL_Rect* _dest, int x, int y, const int _sizeX, const int _sizeY) 
+Ore::Ore(int Id, SDL_Rect* _src, SDL_Rect* _dest, int x, int y, const int _sizeX, const int _sizeY, bool empty) 
 	: Piece(Id, _src, _dest, x, y, _sizeX, _sizeY) {
-	color = make_unique<Colors>(GetRandomColor());
+
+	if (empty)	color = make_unique<Colors>(Colors::EMPTY);
+	else color = make_unique<Colors>(GetRandomColor());
+
 	textureId = ConvertColorToTextureId(*(color.get()));
 }
 
--- PuzzleGame/PuzzleGame/Source/Ore.h
+++ PuzzleGame/PuzzleGame/Source/Ore.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Ore : public Piece {
 public:
-	Ore(int Id, SDL_Rect* _src, SDL_Rect* _dest, int x, int y, const int _sizeX, const int _sizeY);
+	Ore(int Id, SDL_Rect* _src, SDL_Rect* _dest, int x, int y, const int _sizeX, const int _sizeY, bool empty);
 	~Ore();
 
 	const Colors& GetColor() const;
