a89af063254c9b188f6eb07f6890ffc49750a83e
iris-rod <igprodrigues125@gmail.com>
2021-02-21 22:41:21 +0000

    Add new consts

--- PuzzleGame/PuzzleGame/Source/Consts.h
+++ PuzzleGame/PuzzleGame/Source/Consts.h
@@ -1,13 +1,17 @@
 #pragma once
 
-static const int TOTAL_COLUMNS = 15; //10
+static const int TOTAL_COLUMNS = 30; //10
 static const int TOTAL_ROWS = 7; //7
-static const int INITIAL_COLUMNS = 10;
+static const int INITIAL_COLUMNS = 5;
 static const int PIECE_SIZE_X = 32;
 static const int PIECE_SIZE_Y = 32;
 
 static const int NEW_COLUMN_TIME = 6; //in seconds
 static const int SHOW_INFO_TEXT_TIME = 5; //in seconds
 
-static const int START_X = 300;
-static const int START_Y = 50;
+static const int START_X = 240;
+static const int START_Y = 126;
+
+static const int NEW_LEVEL_POINTS_INC = 20;
+static const int STARTING_NEXT_LEVEL_POINTS = 50;
+
